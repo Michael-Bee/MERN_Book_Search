1aea98d028fe6919c9834abc70bf6c8c2055e011
Michael Bee <bee.michael.02@gmail.com>
2022-04-29 10:45:15 -0500

    Made typeDefs, updated resolvers as needed to work together.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,10 +9,6 @@ const resolvers = {
             return User.find();
         },
 
-        user: async (parent, { userId }) => {
-            return User.findOne({ _id: userId });
-        },
-
         me: async (parent, args, context) => {
             if (context.user) {
                 return User.findOne({_id: context.user._id})
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,41 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bookCount: 
+        savedBooks: [Book]!
+    }
+    
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String        
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        profile: Profile
+    }
+    
+    type Query {
+        users: [User]!
+        me: User
+    }
+
+    }
+    type Mutation {
+        createUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        addBook(bookData: BookInput!): User
+        deleteBook(bookId: String!): User
+    }
+`;
+
+module.exports = typeDefs;
