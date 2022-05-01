dfda2a72c7ba763688bf0a739f63e2df8e8fcd8f
Michael Bee <bee.michael.02@gmail.com>
2022-05-01 13:25:55 -0500

    Front end queries done.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,19 @@
-.
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            email
+            savebook {
+                authors
+                description
+                bookId
+                image
+                link
+                title
+            }
+        }
+    }
+`;
