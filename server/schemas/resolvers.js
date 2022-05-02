ab182dcd9a0706ba3c2778d1c08d477f7e2f2d58
Michael Bee <bee.michael.02@gmail.com>
2022-05-01 21:28:51 -0500

    Debugging issues with server.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,15 +5,11 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        users: async () => {
-            return User.find();
-        },
-
         me: async (parent, args, context) => {
             if (context.user) {
                 return User.findOne({_id: context.user._id})
             }
-            throw new AuthenticationError('You need to be logged in!');
+            throw new AuthenticationError('Please log in');
         },
     },
 
@@ -28,13 +24,13 @@ const resolvers = {
             const user = await User.findOne({email});
 
             if (!user) {
-                throw new AuthenticationError('No user with this email found!');
+                throw new AuthenticationError('Incorrect credentials');
             }
 
             const correctPw = await user.isCorrectPassword(password);
 
             if (!correctPw) {
-                throw new AuthenticationError('Incorrect password!');
+                throw new AuthenticationError('Incorrect credentials');
             }
 
             const token = signToken(user);
@@ -52,7 +48,7 @@ const resolvers = {
                     }
                 );
             }
-            throw new AuthenticationError('You need to be logged in!')
+            throw new AuthenticationError('Please log in')
         },
 
         deleteBook: async (parent, { bookId }) => {
@@ -63,7 +59,7 @@ const resolvers = {
                     {new: true}
                 );
             }
-            throw new AuthenticationError('You need to be logged in!')
+            throw new AuthenticationError('Please log in')
         },
     },
 };
--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+const { type, resolvers } = require('./schemas');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
-  typeDefs,
+  type,
   resolvers,
-  context: authMiddleware,
+  context:authMiddleware
 });
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,22 +24,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// Create a new instance of an Apollo server with the GraphQL schema
-const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
-  
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-    })
-  })
-  };
+app.use(routes);
 
-// Call the async function to start the server
-startApolloServer(typeDefs, resolvers);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}${server.graphqlPath}`)
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+  })
+});
